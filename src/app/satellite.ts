a2fd08a3d3b546ea74779b4546a0fc5545caf0a8
lirow150 <lorena.paez0814@gmail.com>
2022-07-31 13:11:29 -0500

    fixed isSpaceDebriscommit

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -14,10 +14,20 @@ export class Satellite {
 		this.operational = operational;
    }
 	
-	isSpaceDebris(): boolean {
-		return true;
-   }
+// this is what must be fixed:
+//	isSpaceDebris(): boolean {
+// 		return true;
+		
+//    }
+// TODO 3a: fix isSpaceDebris check
+shouldShowWarning() {
+	return this.type == "Space Debris";
+	// if this.name = 'Space Debris' {
+	// return this.name === true;
+	// }
+}  
 
 }
 
-// TODO 3a: fix isSpaceDebris check
+
+
